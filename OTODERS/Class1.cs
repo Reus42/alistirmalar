cdf7df1383195a05bce320386917d359071c6d76
Uğur Özşahin <ozsahinqreus@gmail.com>
2023-03-07 13:40:39 +0300

    OTODERS
    Varsayılan internet tarayıcınızda lms üzerinde oturumunuz açık olduğu sürece çalışır.

---
+++ OTODERS/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+public class Win32
+{
+
+    [DllImport("User32.Dll")]
+    public static extern long SetCursorPos(int x, int y);
+
+    [DllImport("User32.Dll")]
+    public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+    }
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void fare_kontrolu(Int32 dwFlags, Int32 dx, Int32 dy, Int32 cButtons, Int32 dwExtraInfo);
+    //Yukarıdaki kodda ise fareyi istediğimiz noktqaya götürüp istediğimiz işlemi yapmamızı sağlıyoruz
+
+    private const int sol_bas = 0x02;
+    private const int sol_kaldir = 0x04;
+    public static void SolTikla()
+    {
+
+        int farenin_x_konumu = Cursor.Position.X;
+        int farenin_y_konumu = Cursor.Position.Y;
+        // Yine aynı şekilde farenin tıklayacağı konumları ayarlıyoruz
+
+       
+        //ve aynı şekilde konum ve tıklanacak tuşların HexaDecimal cinsinden ASCII değerlerini gönderiyoruz ve tıklamasını sağlıyoruz
+    }
+
+
+
+
+
+
+
+
+
+}
+
---
+++ OTODERS/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OTODERS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            timer2 = new System.Windows.Forms.Timer(components);
+            timer4 = new System.Windows.Forms.Timer(components);
+            pageSetupDialog1 = new PageSetupDialog();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(102, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 25);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 31000;
+            timer2.Tick += timer2_Tick;
+            // 
+            // timer4
+            // 
+            timer4.Interval = 3000;
+            timer4.Tick += timer4_Tick;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(12, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(379, 32);
+            label3.TabIndex = 2;
+            label3.Text = "LÜTFEN BİLGİSAYARI KAPATMA ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(461, 188);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer4;
+        private PageSetupDialog pageSetupDialog1;
+        private Label label3;
+    }
+}
